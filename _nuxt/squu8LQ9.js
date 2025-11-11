534852004c4dc90dcfce6b97566483dcbfe30bfb
VictorDelamonica <victor.delamonica@epitech.eu>
2025-11-11 12:56:52 +0000

    Deploying to gh-pages from @ VictorDelamonica/VictorDelamonica.github.io@0fe707d0f9bd815c2040a1fb793fb4a4ed80808b 🚀

--- _nuxt/squu8LQ9.js
+++
@@ -1,1 +0,0 @@
-import{J as a,k as s,K as r,L as u,M as o}from"#entry";function d(t){const e=t||s();return e.ssrContext?.head||e.runWithContext(()=>{if(r()){const n=u(o);if(!n)throw new Error("[nuxt] [unhead] Missing Unhead instance.");return n}})}function i(t,e={}){const n=e.head||d(e.nuxt);return a(t,{head:n,...e})}export{i as u};
