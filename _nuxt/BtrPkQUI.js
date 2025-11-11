7c3b088d6eda659b0ebbb5eaed629f75c3a50735
VictorDelamonica <victor.delamonica@epitech.eu>
2025-11-11 18:36:25 +0000

    Deploying to gh-pages from @ VictorDelamonica/VictorDelamonica.github.io@b7c8d5794718d0a32c75d52afd71f13ccb911a7a 🚀

---
+++ _nuxt/BtrPkQUI.js
@@ -0,0 +1,15 @@
+import{_ as s}from"./Co-SFEto.js";import{_ as a}from"./Hnpsd16S.js";/* empty css        */import{_ as i}from"./DlAUqK2U.js";import{e as u,w as c,o as p,b as t,a as e}from"#entry";import"./Bypc3syH.js";import"./Be1lb-yr.js";const l=`
+Rejoindre IncluSens comme bénévole, c’est choisir de donner de son temps et de son énergie pour faire bouger les lignes de l’inclusion.
+
+Nous recherchons aujourd’hui :
+
+• des patients experts, **formés aux 40 heures d’éducation thérapeutique**, pour **partager leur expérience et accompagner** d’autres patients à travers nos ateliers ;
+
+• des bénévoles motivés, prêts à nous aider à **déployer notre exposition** « Le handicap invisible en entreprise » partout en France, et **à animer des tables rondes** pour sensibiliser le grand public comme les professionnels.
+
+Peu importe le temps que vous pouvez consacrer, chaque contribution a un impact.
+`,m=`
+Contactez nous via notre **formulaire en ligne** ou venez nous rencontrer lors de nos événements. (VOIR HELLO ASSO)
+
+Nous serons ravies d’échanger avec vous et de vous accueillir dans notre équipe.
+`,d={__name:"helper",setup(_){return(v,o)=>{const n=s,r=a;return p(),u(r,{subtitle:"Devenir bénévole","subtitle-style":"handwritten"},{default:c(()=>[t(n,{value:l,tag:"div"}),o[0]||(o[0]=e("p",{class:"text-center"},[e("br"),e("strong",null,"Comment nous rejoindre ?")],-1)),t(n,{value:m,tag:"div"})]),_:1})}}},N=i(d,[["__scopeId","data-v-b6adbeb3"]]);export{N as default};
