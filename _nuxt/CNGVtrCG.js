1afe5f495def63dbcdf8cc5e23de494d7712a880
VictorDelamonica <victor.delamonica@epitech.eu>
2025-11-13 11:13:32 +0000

    Deploying to gh-pages from @ VictorDelamonica/VictorDelamonica.github.io@369f37e6c5ecd50be24623059a3dc9ef4b9b2a36 🚀

---
+++ _nuxt/CNGVtrCG.js
@@ -0,0 +1,13 @@
+import{_ as n}from"./3ggDM222.js";import{_ as o}from"./_uo5x12d.js";import{_ as r}from"./DlAUqK2U.js";import{c as t,w as i,o as a,f as c}from"./COizZV1D.js";const u=`
+IncluSens vise à transformer nos épreuves en actions utiles.
+
+Parce que nous savons ce que signifie chercher de l’aide et ne pas la trouver, nous avons décidé de créer des solutions concrètes pour que d’autres n’aient pas à traverser seuls ce chemin.
+
+Notre mission est d’abord **d’accompagner**. Offrir aux patients et à leurs proches des **espaces où l’on peut parler librement, partager des outils, retrouver confiance en soi mais aussi apprendre à mieux gérer sa fatigue, son stress et sa douleur.**
+
+Notre mission est aussi de **sensibiliser**. Dans les **entreprises mais aussi des particuliers**, dans la société, là où trop souvent le handicap invisible reste méconnu. Nous voulons ouvrir les yeux, **changer les regards et favoriser une meilleure compréhension**.
+
+Enfin, notre mission est de **créer du lien**. Entre la vie personnelle et le monde professionnel, entre les malades et les aidants, entre les individus et la société.
+
+Parce que l’inclusion n’est pas un mot, c’est un chemin que nous construisons ensemble.
+`,l={__name:"missions",setup(m){return(p,d)=>{const e=n,s=o;return a(),t(s,{subtitle:"Nos missions","subtitle-style":"handwritten"},{default:i(()=>[c(e,{value:u,tag:"div"})]),_:1})}}},g=r(l,[["__scopeId","data-v-0fc8db92"]]);export{g as default};
